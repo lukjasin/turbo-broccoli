1baff7a80ad0291158760600ef2ce5c48169b241
lukjasin <luk.jasin@gmail.com>
2024-06-14 09:50:17 +0200

    init

---
+++ tutorial1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is a main page <h1>HELLO</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    app.run()
